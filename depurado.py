63f9832b3c69fd510cb1898d87a0d59dacb3f9de
EFrascarelli <efrascarelli@fi.uba.ar>
2023-10-16 20:06:34 -0300

    Toques finales

--- depurado.py
+++ depurado.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
+valid_chars = "abcdefghijklmnopqrstuvwxyz ."
+
 def verify_string(string):
+    # This function will receive every value from a cell which is a string and delete not wanted characters
+    # It will return text strings whose characters are valid
     new_str = string.lower()
     for char in new_str:
         try:
@@ -12,37 +16,46 @@ def verify_string(string):
     return string
 
 def data_depuration(df):
+    # This function will iterate the Dataframe and will check every value that is a string
     for columnName in df:
         if (df[columnName].dtype == object):
             df[columnName] = df[columnName].apply(verify_string)
     return
 
 def get_max_value(group, value):
-    max_value_group = merged_data.groupby(group)[value].sum().idxmax()
-    max_value = merged_data.groupby(group)[value].sum().max()
+    # This function will receive a group and the value from which you want to obtain the maximum.
+    # It will return the element from the group with the highest value, and that value.
+    max_value_group = merged_df.groupby(group)[value].sum().idxmax()
+    max_value = merged_df.groupby(group)[value].sum().max()
     return (max_value_group, max_value)
 
 def get_min_value(group, value):
-    min_value_group = merged_data.groupby(group)[value].sum().idxmin()
-    min_value = merged_data.groupby(group)[value].sum().min()
+    # This function will receive a group and the value from which you want to obtain the minimum.
+    # It will return the element from the group with the lowest value, and that value.
+    min_value_group = merged_df.groupby(group)[value].sum().idxmin()
+    min_value = merged_df.groupby(group)[value].sum().min()
     return (min_value_group, min_value)
-
-valid_chars = "abcdefghijklmnopqrstuvwxyz ."
 
 df = pd.read_csv('media_source.csv')
 df2 = pd.read_csv('mmp.csv')
 
-merged_data = pd.merge(df, df2, on=["date", "app_id", "campaign_id", "campaign_name", "creative_name", "country"])
 
-merged_data["CPI"] = merged_data["spend_x"] / merged_data["installs"]
-merged_data["CPC"] = merged_data["spend_y"] / merged_data["clicks"]
+# ------ Mergeo ---------
+merged_df = pd.merge(df, df2, on=["date", "app_id", "campaign_id", "campaign_name", "creative_name", "country"])
 
+
+# ------ Creacion de variables -------
+merged_df["CPI"] = merged_df["spend_x"] / merged_df["installs"]
+merged_df["CPC"] = merged_df["spend_y"] / merged_df["clicks"]
+
+# ------ Depuracion de los dataframes ---------
 data_depuration(df)
 data_depuration(df2)
 
-print(merged_data)
+print(merged_df)
 
+# ------- Respuestas -------
 print("El pais con mas gasto es %s, con un gasto de %s" % get_max_value("country","spend_x"))
-print("La campaña con mejor performance es %s, con un CPI de %s" % get_min_value("campaign_name","CPI"))
-print("La campaña con más installs es %s, con %s installs" % get_max_value("campaign_name","installs"))
-print("El día con más installs es %s, con %s installs" % get_max_value("date","installs"))
+print("La campania con mejor performance es %s, con un CPI de %s" % get_min_value("campaign_name","CPI"))
+print("La campania con mas installs es %s, con %s installs" % get_max_value("campaign_name","installs"))
+print("El dia con mas installs es %s, con %s installs" % get_max_value("date","installs"))
